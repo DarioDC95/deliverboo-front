eea725bfbb03a16525bc6dab8b4a3f8a053e4f71
DarioDC95 <dario_95@live.com>
2023-03-28 17:58:59 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import "/node_modules/bootstrap/dist/css/bootstrap.css"
+import * as bootstrap from 'bootstrap'
+import { router } from './router.js'
+
+createApp(App).use(router).mount('#app')
