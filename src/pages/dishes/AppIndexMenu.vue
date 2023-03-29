1c7ea25b3fde87da1cac7f5168f7a853ca25e750
DarioDC95 <dario_95@live.com>
2023-03-29 18:54:20 +0200

    fixed AppIndexDishes

--- src/pages/dishes/AppIndexMenu.vue
+++ src/pages/dishes/AppIndexMenu.vue
@@ -14,7 +14,6 @@ export default {
     methods:{
         getDishes(){
             axios.get(`${store.url_restaurants}api/dishes/${this.$route.params.id}`).then((response) => {
-                console.log(response)
                 if (response.data.success) {
                     store.dishes_by_restaurant = response.data.result;
                     store.loading = false;
@@ -33,8 +32,36 @@ export default {
 <template>
     <main>
         <section>
-            <div >
-                
+            <div class="container">
+                <div class="row">
+                    <div v-for="(dish, index) in store.dishes_by_restaurant" :key="index" class="col-3">
+                        <div class="card h-100">
+                            <img :src="dish.image_path == null ? 'https://picsum.photos/id/1/200/300' : `${store.url_restaurants}storage/${dish.image_path}`" :alt="dish.name" class="card-img-top">
+                            <div class="card-body d-flex flex-column">
+                                <div>
+                                    <h4>{{ dish.name }}</h4>
+                                    <h6>Via: {{ dish.vegan }}</h6>
+                                </div>
+                                <div class="flex-grow-1 d-flex flex-column justify-content-between">
+                                    <div>
+                                        <p>
+                                            <em><strong>Tipologia:</strong></em>
+                                        </p>
+                                        <div>
+                                            <!-- <span class="badge bg-success mx-1" v-for="(type, index) in restaurant.types" :key="index">{{type.name}}</span> -->
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div>
+                                            QUESTO E' IL BUTTON PER AGGIUNGERLO AL CARRELLO
+                                            <!-- <router-link :to="{ name: 'dishes', params: { id: restaurant.id } }" class="btn btn-secondary">Vai al Menù</router-link>  -->
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </main>
