f6bda5f2b24073f3e6610620cc1fe43e3c3d1205
fedeloxe <loxeee@gmail.com>
2023-03-29 12:43:07 +0200

    merge

---
+++ src/pages/restaurants/AppIndexRestaurants.vue
@@ -0,0 +1,8 @@
+<script>
+// import RestaurantsCard from '../components/RestaurantsCard.vue';
+export default {
+
+}
+</script>
+<template></template>
+<style lang="scss" scoped></style>
--- src/router.js
+++ src/router.js
@@ -4,20 +4,26 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import AppHomepage from './pages/AppHomepage.vue';
 import AppFailed404 from './components/AppFailed404.vue';
+import AppIndexRestaurants from './pages/restaurants/AppIndexRestaurants.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-      {
-        name: 'homepage',
-        path: '/',
-        component: AppHomepage
-      },
-      {
-        path: '/:path*',  /* il ":whatever*" diventa un selettore universale di tutte le pagine che non dovessero corrispondere con le rotte qui presenti*/
-        name: 'failed',
-        component: AppFailed404
-      },
+    {
+      name: 'homepage',
+      path: '/',
+      component: AppHomepage
+    },
+    {
+      path: '/:path*',  /* il ":whatever*" diventa un selettore universale di tutte le pagine che non dovessero corrispondere con le rotte qui presenti*/
+      name: 'failed',
+      component: AppFailed404
+    },
+    {
+      name: 'AppIndexRestaurants',
+      path: '/restaurants',
+      component: AppIndexRestaurants
+    }
   ]
 })
 
