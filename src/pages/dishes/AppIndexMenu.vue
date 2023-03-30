9063f1d48939b95ec19c1cdd8c129fe25060b465
fedeloxe <loxeee@gmail.com>
2023-03-30 12:42:03 +0200

    img dishes fixed

--- src/pages/dishes/AppIndexMenu.vue
+++ src/pages/dishes/AppIndexMenu.vue
@@ -1,5 +1,5 @@
 <script>
-import {store} from '../../store';
+import { store } from '../../store';
 import axios from 'axios';
 
 import AppLoader from '../../components/AppLoader.vue';
@@ -8,23 +8,23 @@ export default {
     components: {
         AppLoader,
     },
-    data(){
-        return{
+    data() {
+        return {
             store
 
         }
     },
-    mounted(){
+    mounted() {
         this.getDishes()
     },
-    methods:{
-        getDishes(){
+    methods: {
+        getDishes() {
             store.loading = true;
             axios.get(`${store.url_restaurants}api/dishes/${this.$route.params.id}`).then((response) => {
                 if (response.data.success) {
                     store.dishes_by_restaurant = response.data.result;
                     store.loading = false;
-                    
+
                     console.log(store.dishes_by_restaurant)
                 }
                 else {
@@ -32,9 +32,9 @@ export default {
                 }
             })
         },
-        addCart(dish){
-           store.cart.push(dish)
-           console.log(store.cart)
+        addCart(dish) {
+            store.cart.push(dish)
+            console.log(store.cart)
         }
 
         // removeCart(dish){
@@ -46,14 +46,16 @@ export default {
 </script>
 
 <template>
-    <AppLoader v-if="store.loading"/>
+    <AppLoader v-if="store.loading" />
     <main v-else>
         <section class="menu">
             <div class="container">
                 <div class="row mt-3 gy-4">
-                    <div v-for="(dish, index) in store.dishes_by_restaurant" :key="index" class="col-12 col-sm-6 col-md-3" :class="dish.visible ? '' : 'd-none'">
+                    <div v-for="(dish, index) in store.dishes_by_restaurant" :key="index" class="col-12 col-sm-6 col-md-3"
+                        :class="dish.visible ? '' : 'd-none'">
                         <div class="card h-100">
-                            <img :src="dish.image_path == null ? 'https://picsum.photos/id/1/200/300' : `${store.url_restaurants}storage/${dish.image_path}`" :alt="dish.name" class="card-img-top">
+                            <img :src="dish.image_path == '' ? '../../../img/logo-blue.png' : `${store.url_restaurants}storage/${dish.image_path}`"
+                                :alt="dish.name" class="card-img-top">
                             <div class="card-body d-flex flex-column">
                                 <div>
                                     <h4>{{ dish.name }}</h4>
@@ -117,7 +119,8 @@ export default {
                                     </div>
                                     <div>
                                         <div>
-                                            <button @click="addCart(dish)" class="btn btn-primary btn-sm"><i class="fa-solid fa-plus"></i></button>
+                                            <button @click="addCart(dish)" class="btn btn-primary btn-sm"><i
+                                                    class="fa-solid fa-plus"></i></button>
                                             <!-- <button @click="removeCart(dish)" class="btn btn-danger btn-sm"><i class="fa-solid fa-minus"></i></button> -->
                                             QUESTO E' IL BUTTON PER AGGIUNGERLO AL CARRELLO
                                         </div>
@@ -133,42 +136,43 @@ export default {
 </template>
 
 <style scoped lang="scss">
-    .menu {
+.menu {
 
-        .icons {
+    .icons {
 
-            .vegan, .no-vegan {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                font-size: 1.5rem;
-                min-width: 40px;
-                min-height: 40px;
-                border-radius: 50%;
-                
-                .fa-solid {
-                    color: green;
-                }
+        .vegan,
+        .no-vegan {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 1.5rem;
+            min-width: 40px;
+            min-height: 40px;
+            border-radius: 50%;
+
+            .fa-solid {
+                color: green;
             }
+        }
 
-            .vegan {
-                border: 3px solid green;
-            }
+        .vegan {
+            border: 3px solid green;
+        }
 
-            .no-vegan {
-                position: relative;
-                border: 3px solid red;
+        .no-vegan {
+            position: relative;
+            border: 3px solid red;
 
-                .slash {
-                    height: 5px;
-                    width: 100%;
-                    background-color: red;
-                    position: absolute;
-                    top: 50%;
-                    left: 50%;
-                    transform: translate(-50%, -50%) rotate(45deg);
-                }
+            .slash {
+                height: 5px;
+                width: 100%;
+                background-color: red;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%) rotate(45deg);
             }
         }
     }
+}
 </style>
